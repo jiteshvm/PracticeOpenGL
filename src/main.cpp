0afb055b8fbf4aeb294f92a27e30409791e0ae46
Jitesh Mulchandani <jiteshvm@gmail.com>
2017-11-15 18:13:36 -0800

    use dynamically linked CRT option draw rectangle using Element Buffer Objects

--- src/main.cpp
+++ src/main.cpp
@@ -46,22 +46,33 @@ int main()
 
 	Shader ourShader("./shaders/basic.vs", "./shaders/basic.frag");
 
-	std::cout << "size : " << sizeof(short);
-
 	float vertices[] = {
 		// positions         // colors
-		1.0f , -1.0f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-		-1.0f, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		0.0f ,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+		0.5f , -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+		-0.5f, 0.5f , 0.0f,  0.0f, 0.0f, 1.0f,  // top left
+		0.5f , 0.5f , 0.0f,  1.0f, 1.0f, 1.0f   // top right
 	};
 
-	unsigned int VBO, VAO;
+	unsigned int indices[] = {
+		0, 1, 2,
+		0, 2, 3
+	};
+
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
 
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -72,9 +83,9 @@ int main()
 
 	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	// glBindVertexArray(0);
+	glBindVertexArray(0);
 
-
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -84,7 +95,8 @@ int main()
 
 		ourShader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
