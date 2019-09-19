fbbfded7cbb3cb02c4f5a3c86227c83c86976b98
Jitesh <jiteshvm@gmail.com>
2019-09-19 03:46:03 -0700

    camera wip

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,13 @@ using namespace std;
 
 const int window_width = 800;
 const int window_height = 600;
+float cameraSpeed = 0.05f;
 GLFWwindow* window;
 Cube* cubes[10];
+
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 
 void Init();
 void UpdateLoop();
@@ -111,6 +116,15 @@ void ProcessInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 void UpdateLoop()
